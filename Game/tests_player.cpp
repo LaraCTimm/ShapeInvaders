1221a4d9202701208c8013e2d79081859eb22561
LaraCTimm <l_timm@hotmail.com>
2017-09-24 11:01:42 +0200

    Updated test files with pointer function calls

--- Game/tests_player.cpp
+++ Game/tests_player.cpp
@@ -13,21 +13,21 @@ TEST_CASE("Check GameObject vector size = 1 at start") {
 
 TEST_CASE("Check Game starts with a player object") {
 	Game newGame(1);
-	CHECK( newGame.getGameObjectsVector()[0].getObjectType() == gameObjectType::Player);
+	CHECK( newGame.getGameObjectsVector()[0]->getObjectType() == gameObjectType::Player);
 }
 
 TEST_CASE("Check Player is instantiated at (700,400)"){
 	Game newGame(1);
-	CHECK(newGame.getGameObjectsVector()[0].getXCoord() == 700);
-	CHECK(newGame.getGameObjectsVector()[0].getYCoord() == 400);
+	CHECK(newGame.getGameObjectsVector()[0]->getXCoord() == 700);
+	CHECK(newGame.getGameObjectsVector()[0]->getYCoord() == 400);
 }
 
 
 TEST_CASE("Check player can move in anti-clockwise direction"){
 	Game newGame(5);
-	int oldx = newGame.getGameObjectsVector()[0].getXCoord(), oldy = newGame.getGameObjectsVector()[0].getYCoord();
+	int oldx = newGame.getGameObjectsVector()[0]->getXCoord(), oldy = newGame.getGameObjectsVector()[0]->getYCoord();
 	newGame.movePlayerObject(1);
-	int newx = newGame.getGameObjectsVector()[0].getXCoord(), newy = newGame.getGameObjectsVector()[0].getYCoord();
+	int newx = newGame.getGameObjectsVector()[0]->getXCoord(), newy = newGame.getGameObjectsVector()[0]->getYCoord();
 	CHECK(oldx > newx);
 	CHECK(oldy < newy);
 }
@@ -35,9 +35,9 @@ TEST_CASE("Check player can move in anti-clockwise direction"){
 
 TEST_CASE("Check player can move in clockwise direction"){
 	Game newGame(5);
-	int oldx = newGame.getGameObjectsVector()[0].getXCoord(), oldy = newGame.getGameObjectsVector()[0].getYCoord();
+	int oldx = newGame.getGameObjectsVector()[0]->getXCoord(), oldy = newGame.getGameObjectsVector()[0]->getYCoord();
 	newGame.movePlayerObject(-1);
-	int newx = newGame.getGameObjectsVector()[0].getXCoord(), newy = newGame.getGameObjectsVector()[0].getYCoord();
+	int newx = newGame.getGameObjectsVector()[0]->getXCoord(), newy = newGame.getGameObjectsVector()[0]->getYCoord();
 	CHECK(oldx > newx);
 	CHECK(oldy > newy);
 }
@@ -45,29 +45,29 @@ TEST_CASE("Check player can move in clockwise direction"){
 TEST_CASE("Check bullet can be instantiated"){
 	Game newGame(5);
 	int oldSize = newGame.getGameObjectsVector().size();
-	newGame.AddGameObject(gameObjectType::PlayerBullet);
+	newGame.AddGameObject(gameObjectType::PlayerBullet, 1);
 	CHECK(newGame.getGameObjectsVector().size() == oldSize + 1);
 }
 
 TEST_CASE("Check bullet is of type 'PlayerBullet'"){
 	Game newGame(5);
-	newGame.AddGameObject(gameObjectType::PlayerBullet);
-	CHECK(newGame.getGameObjectsVector()[newGame.getGameObjectsVector().size()-1].getObjectType() == gameObjectType::PlayerBullet);
+	newGame.AddGameObject(gameObjectType::PlayerBullet, 1);
+	CHECK(newGame.getGameObjectsVector()[newGame.getGameObjectsVector().size()-1]->getObjectType() == gameObjectType::PlayerBullet);
 }
 // Check bullet move
 
 TEST_CASE("Check enemy can be instantiated"){
 	Game newGame(5);
 	int oldSize = newGame.getGameObjectsVector().size();
-	newGame.AddGameObject(gameObjectType::Enemy);
+	newGame.AddGameObject(gameObjectType::Enemy, 1);
 	CHECK(newGame.getGameObjectsVector().size() == oldSize + 1);
 }
 
 TEST_CASE("Check multiple enemies can be instantiated"){
 	Game newGame(5);
 	int oldSize = newGame.getGameObjectsVector().size();
-	newGame.AddGameObject(gameObjectType::Enemy);
-	newGame.AddGameObject(gameObjectType::Enemy);
-	newGame.AddGameObject(gameObjectType::Enemy);
+	newGame.AddGameObject(gameObjectType::Enemy, 1);
+	newGame.AddGameObject(gameObjectType::Enemy, 1);
+	newGame.AddGameObject(gameObjectType::Enemy, 1);
 	CHECK(newGame.getGameObjectsVector().size() == oldSize + 3);
 }
