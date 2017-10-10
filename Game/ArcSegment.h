389d36167e58292290dcdbe8d7b014b2cb1020b6
Sasha Berkowitz <sashabii@hotmail.com>
2017-10-10 15:49:40 +0200

    Doxygen descriptions added to arc segment header file

--- Game/ArcSegment.h
+++ Game/ArcSegment.h
@@ -4,12 +4,23 @@
 #include "GameObject.h"
 #include "Game.h"
 
+/**
+ * ArcSegment class - represents segments making up arc between laser generator pair
+ * Inherits from GameObject class
+ */
 class ArcSegment : public GameObject
 {
 public:
-
+/**
+ * @brief Arc segment constructor.
+ * @param Angle along which the segment moves
+ * @param ID unique to each set of laser generators with joining arc
+ */
 ArcSegment(float angle, int ID);
-
+/**
+ * @brief Gets ID assigned to segment
+ * @return Segment's ID 
+ */
 int getID() { return _ID; };
 
 private:
